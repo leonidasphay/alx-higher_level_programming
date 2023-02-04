35b5ef95518bc0165f6354f4d83ea1fc5bf6a169
Favour <favourkolawole21@gmail.com>
2023-02-04 13:12:21 +0100

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,11 @@
-# strace - fixed .phpp in wp-settings.php
-exec { 'fixed-phpp':
-  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path    => '/bin';
+#chage name of the file to fix error 500
+exec { '/bin/bash':
+  command => 'sudo mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp',
+  creates => '/var/www/html/wp-includes/class-wp-locale.phpp',
+  path    => ['/usr/bin', '/usr/sbin'],
+}
+
+service { 'apache2':
+  ensure  => running,
+  restart => 'sudo service apache2 restart',
 }
